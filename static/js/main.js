634bf24265155e1952f2ffca91b28cffc916eadf
SergioAnd95 <developinc@yadnex.ru>
2017-10-04 12:17:16 +0300

    add scroll to tab about site

--- static/js/main.js
+++ static/js/main.js
@@ -181,5 +181,14 @@ $(document).ready(function(){
 		});
     })
 
+	$("a.topLink").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+         duration: 500,
+         easing: "swing"
+      });
+      return false;
+   });
 
 });
